d361bc5098295f4700efeb89bacf576a31c1cb7a
TruongNBN <truongnbn.main@gmail.com>
2024-05-15 14:29:47 +0700

    merge main

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import LogoSVG from "~/components/images/LogoSVG";
 import { PagePath } from "~/enums/path";
 
 const Logo = () => {
 	return (
-		<Link href={PagePath.home} className="self-center text-2xl font-semibold whitespace-nowrap">
+		<Link href={PagePath.home} className="flex items-center gap-2 self-center text-xl font-semibold whitespace-nowrap">
+			<LogoSVG size={30} />
 			<span className="text-primary-600">Speech Convert</span>
 		</Link>
 	);
