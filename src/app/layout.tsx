f77d0bc618500f791aeda6b8f45b3db38c3fedac
TruongNBN <truongnbn.main@gmail.com>
2024-05-17 17:05:03 +0700

    add lib next/third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
 import "~/styles/global.scss";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { ThemeProvider } from "~/contexts/ThemeContext";
 
 export const metadata: Metadata = {
@@ -18,22 +18,11 @@ const RootLayout = ({ children }: React.PropsWithChildren) => {
 					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 					<link rel="manifest" href="/site.webmanifest" />
-					<Script async src="https://www.googletagmanager.com/gtag/js?id=G-NW2E273HT2" />
-					<Script
-						id={"gtag"}
-						dangerouslySetInnerHTML={{
-							__html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-NW2E273HT2');
-              `,
-						}}
-					/>
 				</head>
 				<body className="bg-gray-50 dark:bg-gray-900 overflow-x-hidden">
 					<div className="h-screen md:max-w-screen-xl mx-auto">{children}</div>
 				</body>
+				<GoogleAnalytics gaId="G-NW2E273HT2" />
 			</html>
 		</ThemeProvider>
 	);
