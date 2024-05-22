4b3549042f46b4e0ef227855632a2a86b1378d42
TruongNBN <truongnbn.main@gmail.com>
2024-05-23 00:13:00 +0700

    redirect home to textToSpeech

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+// import React from "react";
+import { redirect } from "next/navigation";
+import { PagePath } from "~/enums/path";
 // import HomePage from "~/components/home/HomePage";
 
-const Page = () => {
-	return (
-		<div className="h-screen mx-auto">
-			{/* <HomePage /> */}
-			Home
-		</div>
-	);
+const Page = async () => {
+	redirect(PagePath.textToSpeech);
+	// return (
+	// 	<div className="h-screen mx-auto">
+	// 		{/* <HomePage /> */}
+	// 		Home
+	// 	</div>
+	// );
 };
 
 export default Page;
