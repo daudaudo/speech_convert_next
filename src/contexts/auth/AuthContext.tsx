869419474a8a31b3cb339f5988ed415e663508b5
TruongNBN <truongnbn.main@gmail.com>
2024-06-10 20:00:22 +0700

    chore: handle redirect to signin page when logout

--- src/contexts/auth/AuthContext.tsx
+++ src/contexts/auth/AuthContext.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React, { createContext, useContext, useState } from "react";
+import { useRouter } from "next/navigation";
 import { getAuthUserByTokenCookie } from "~/actions/usecase/auth/user";
 import { removeToken } from "~/actions/cookies/auth";
 import { AuthenticatedUser } from "~/types/auth";
 import { NotImplementedLogicError } from "~/errors/logic";
+import { PagePath } from "~/enums/path";
 
 type AuthContext = {
 	signout(): Promise<any>;
@@ -23,12 +25,14 @@ interface Props {
 }
 
 export const AuthProvider = ({ children, user: initUser }: Props) => {
+	const router = useRouter();
 	const [user, setUser] = useState<AuthenticatedUser | undefined>(initUser);
 
 	const signout = async () => {
 		try {
 			await removeToken();
 			setUser(undefined);
+			router.replace(PagePath.signin);
 		} catch {}
 	};
 
