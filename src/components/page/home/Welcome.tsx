208884c6a29467ab77556b759093553c323b66da
TruongNBN <truongnbn.main@gmail.com>
2024-06-16 11:52:09 +0700

    fix eslint

--- src/components/page/home/Welcome.tsx
+++ src/components/page/home/Welcome.tsx
@@ -2,7 +2,6 @@
 
 import { useTranslations } from "next-intl";
 import React from "react";
-import SvgIcon from "~/components/icon/SvgIcon";
 
 const Welcome = () => {
 	const t = useTranslations("app");
@@ -13,9 +12,6 @@ const Welcome = () => {
 		>
 			<div className="absolute inset-0 bg-black bg-opacity-80" />
 			<div className="relative inline-flex flex-col justify-center items-center h-full bg-transparent">
-				{/* <div className="w-24 h-24 bg-gray-100 flex justify-center items-center rounded-full border-2 border-primary-500">
-					<SvgIcon name="logo" className="w-16" />
-				</div> */}
 				<div className="w-full whitespace-nowrap font-thin text-center md:text-5xl text-3xl text-gray-100 p-4">
 					{t("title")}
 				</div>
