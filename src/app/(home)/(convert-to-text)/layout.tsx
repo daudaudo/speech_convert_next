66b7452a5462d20648fedb6778b84109d9de6872
TruongNBN <truongnbn.main@gmail.com>
2024-07-13 17:52:59 +0700

    update select language and layout in speech-to-text module

--- src/app/(home)/(convert-to-text)/layout.tsx
+++ src/app/(home)/(convert-to-text)/layout.tsx
@@ -5,7 +5,11 @@ interface Props {
 }
 
 const ConvertToTextLayout = ({ children }: Props) => {
-	return <>{children}</>;
+	return (
+		<div className="w-full rounded-lg divide-y divide-gray-200 dark:divide-gray-800 ring-1 ring-gray-200 dark:ring-gray-800 bg-white dark:bg-gray-900 px-4 py-5 sm:p-6 !p-0 my-4">
+			{children}
+		</div>
+	);
 };
 
 export default ConvertToTextLayout;
--- src/components/ctt/LanguageSelect.tsx
+++ src/components/ctt/LanguageSelect.tsx
@@ -2,15 +2,18 @@
 
 import React from "react";
 import { Option, Select } from "@material-tailwind/react";
+import { useTranslations } from "next-intl";
 import { LanguageCode } from "~/enums/language";
-import { Languages, supportedLanguages } from "~/constants/language";
+import { Languages } from "~/constants/language";
 
 interface Props {
 	language: LanguageCode;
 	setLanguage: (language: LanguageCode) => void;
+	options: LanguageCode[];
 }
 
-const LanguageSelect = ({ setLanguage, language }: Props) => {
+const LanguageSelect = ({ setLanguage, language, options = [] }: Props) => {
+	const t = useTranslations("ctt");
 	const onSelectChange = (value?: string) => {
 		if (!value) return;
 		setLanguage(value as LanguageCode);
@@ -18,15 +21,21 @@ const LanguageSelect = ({ setLanguage, language }: Props) => {
 
 	const renderSelectedOptions = () => {
 		return (
-			<span className="w-full flex items-center gap-1.5 rounded-md font-medium text-sm focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 disabled:cursor-not-allowed disabled:opacity-75 mb-3">
+			<span className="w-full flex items-center gap-1.5 rounded-md font-medium text-sm focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 disabled:cursor-not-allowed disabled:opacity-75">
 				{Languages[language].name}
 			</span>
 		);
 	};
 
 	return (
-		<Select size="md" variant="static" value={language} selected={renderSelectedOptions} onChange={onSelectChange}>
-			{supportedLanguages.map((lang: LanguageCode) => (
+		<Select
+			size="md"
+			label={t("selectLanguegeLabel")}
+			value={language}
+			selected={renderSelectedOptions}
+			onChange={onSelectChange}
+		>
+			{options.map((lang: LanguageCode) => (
 				<Option key={lang} value={lang}>
 					<span className="w-full flex items-center gap-1.5 rounded-md font-medium text-sm focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 disabled:cursor-not-allowed disabled:opacity-75">
 						{Languages[lang].name}
